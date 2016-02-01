678c3ebdd3d9175e8e8a40774ae486c1aa419c2b
Loic Messal <loic.messal@orange.fr>
2016-02-01 16:06:22 +0100

    Creation de compte + fichier map

---
+++ Client/map_google.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <title>
+			Carte Google
+        </title>
+		<link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    
+        <div id="map"></div>
+        
+        
+        <script src="http://maps.googleapis.com/maps/api/js"></script>
+        <script src="test_fog_war.js"></script>
+  </body>
+</html>
+
+
+
+
