c5ce300b71190d781a5ac326d43c46c899d178b6
maylistdls <maylis.tdls@gmail.com>
2016-02-26 18:23:41 +0100

    Bouton et jauge
    je les ai mis en vertical par rapport à la carte à cause de
    l'utilisation verticale du téléphone.

--- Client/map_google.php
+++ Client/map_google.php
@@ -10,7 +10,11 @@
 		
     </head>
     <body>
-    
+    	<div id="jauges">
+    		<p>capacite :<progress id="avancement" value="50" max="100"></progress> </p>
+    	</div>
+
+
         <div id="map"></div>
         <div id="infoposition"></div>
         <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=geometry"></script>
@@ -22,6 +26,14 @@
 			<div>Tilt-back : <span id="tiltFB"></span></div>
 			<div>Inclinaison gauche - droite : <span id="tiltLR"></span></div>
 			<div>Orientation : <span id="direction"></span></div>
+		</div>
+
+		<div id="boutons">
+			<button id="obs" style="background-color:#98FB98" >Observation</button>
+			<button id="assaut" style="background-color:#B0E0E6">Assaut</button>
+			<button id="tir" style="background-color:#F4A460">Tir</button>
+			<button id="protection" style="background-color:#FFD700">Protection</button>
+			<button id="recup" style="background-color:#CD5C5C">Récupération</button>
 		</div>
 		
     </body>
