c0361ff662bb06f856b43280c90f88c496049e33
Loic Messal <loic.messal@orange.fr>
2016-02-29 22:33:31 +0100

    Solution synchronisation client trouvee

---
+++ Client/Loic/testSynchronisation/syncClient.js
@@ -0,0 +1,26 @@
+
+var HeureServeurOffset = new Date();
+HeureServeurOffset = (Math.floor(HeureServeurOffset.getTime()/1000)+5)*1000; // Delai de 25 secondes 
+
+
+function boucle2(){
+    var now = new Date();
+    now = Math.floor(now.getTime()/1000)*1000
+    if (now > HeureServeurOffset)
+    {
+        console.log("time's up");
+        return false;
+    }
+    return true;
+}
+function test(){
+    console.log(new Date);
+    
+    console.log("+10");
+    window.setTimeout(function(){test();},10000);
+}
+window.addEventListener('load',function (e){
+    console.log(new Date())
+    while(boucle2()){};
+    test();
+    } ,false);
---
+++ Client/Loic/testSynchronisation/syncClient.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <title>
+			Carte Google Orientée
+        </title>
+		
+    </head>
+    <body>
+    	
+		<script src="syncClient.js"></script>
+		
+    </body>
+</html>
+
+
+
+
