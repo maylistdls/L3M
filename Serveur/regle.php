41d17f87ce3f7ab829a089e63af0ee9af09f44fd
Maxime <maxime.quenon@gmail.com>
2016-03-03 18:14:08 +0100

    Réalisation des fonctions avancée. Pour précision sur les besoins me contacter.

--- Serveur/regle.php
+++ Serveur/regle.php
@@ -2,41 +2,43 @@
 
 try {
     require 'connexion.php';
-
-    $_POST['id'] = 6;
-    $_POST['etat'] = 'tir';
-
     if ($_POST['etat'] == 'obs') {
         $stmt = $db->prepare('UPDATE perso SET obs=obs*1.5, regen=capa*0.25 WHERE id=:id');
         $stmt->bindParam(':id', $_POST['id']);
         $stmt->execute();
     } elseif ($_POST['etat'] == 'assaut') {
-        /*$sq1 = "SELECT Indice FROM Question WHERE id='".$_POST['selection']."'";
-    if ($result = mysqli_query($link, $sq1)) {
-        while ($ligne = mysqli_fetch_assoc($result)) {
-            $ind = $ligne['Indice'];
-            echo '<p>'.utf8_encode($ind).'</p>';
-        }
-    }*/
-    } elseif ($_POST['etat'] == 'tir') {
-        $stmt = $db->prepare('UPDATE perso SET tir=100/
-          (loc<->(SELECT loc FROM perso WHERE id=:id))
-           WHERE id=
-           (SELECT id FROM perso WHERE
-             (loc<->(SELECT loc FROM perso WHERE id=:id)<=100))');
+        $stmt = $db->prepare('UPDATE perso SET regen=capa*0.25 WHERE id=:id');
         $stmt->bindParam(':id', $_POST['id']);
         $stmt->execute();
-    } elseif ($_POST['etat'] == 'protec') {
-        /*$sq1="SELECT Quest,id FROM Question WHERE Theme='".$_POST['theme']."' AND id<>'".$_POST['selection']."' ORDER BY RAND() LIMIT 1;";
-    if($result = mysqli_query($link, $sq1)){
-        while($ligne = mysqli_fetch_assoc($result)){
-            $qu=$ligne["Quest"];
-            $id=$ligne["id"];
-            echo "<p value=".$id.">".utf8_encode($qu)."</p>";
+        $stmt = $db->prepare('UPDATE perso SET assaut=capa/5 WHERE id=(SELECT id FROM perso WHERE min(loc<->(SELECT loc FROM perso)))');
+        $stmt->execute();
+    } elseif ($_POST['etat'] == 'tir') {
+        $stmt = $db->prepare('UPDATE perso SET regen=capa*0.25 WHERE id=:id');
+        $stmt->bindParam(':id', $_POST['id']);
+        $stmt->execute();
+        $stmt = $db->prepare('SELECT id FROM perso WHERE
+           (loc<->(SELECT loc FROM perso WHERE id=:id)<=100) AND
+            (loc<->(SELECT loc FROM perso WHERE id=:id)>0) AND
+             equipe!=(SELECT equipe FROM perso WHERE id=:id) AND
+              degrees(acos((loc<->(SELECT loc FROM perso WHERE id=:id))/100))<=(:cap-45)%(360) AND
+              degrees(acos((loc<->(SELECT loc FROM perso WHERE id=:id))/100))>=(:cap+45)%(360)');
+        $stmt->bindParam(':id', $_POST['id']);
+        $stmt->bindParam(':cap', $_POST['cap']);
+    		$stmt->execute();
+    		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		$rows = $stmt->fetchAll();
+        foreach($rows as $row) {
+          $up = $db->prepare('UPDATE perso SET tir=100/(loc<->(SELECT loc FROM perso WHERE id=:id)) WHERE id=:ad');
+          $up->bindParam(':id', $_POST['id']);
+          $up->bindParam(':ad', $row[id], PDO::PARAM_INT);
+          $up->execute();
         }
-    }*/
+    } elseif ($_POST['etat'] == 'protec') {
+      $stmt = $db->prepare('UPDATE perso SET protec=0.5, regen=capa*0.25 WHERE id=:id');
+      $stmt->bindParam(':id', $_POST['id']);
+      $stmt->execute();
     } elseif ($_POST['etat'] == 'recup') {
-        $stmt = $db->prepare('UPDATE perso SET recup=capa*10 WHERE id=:id');
+        $stmt = $db->prepare('UPDATE perso SET recup=capa*3,assaut=assaut*10,tir=tir*10 WHERE id=:id');
         $stmt->bindParam(':id', $_POST['id']);
         $stmt->execute();
     }
