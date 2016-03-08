1df5efd7e36b875b6e800cc2d303a3d0678ba382
Loic Messal <loic.messal@orange.fr>
2016-03-08 15:37:40 +0100

    Gestion des QG et connexion

--- Serveur/qg.php
+++ Serveur/qg.php
@@ -4,22 +4,21 @@ try {
     require 'connexion.php';
     $xg=0;
     $yg=0;
-    $_POST['id']=1;
-    $_POST['partie']=1;
     $stmt = $db->prepare('UPDATE sync SET d_sync=d_sync+1');
     $stmt->execute();
     $stmt = $db->prepare('SELECT * FROM sync');
     $stmt->execute();
     $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+	$id = $rows['d_sync'];
     while ($rows['d_sync']<5) {
       sleep(10);
       $stmt = $db->prepare('SELECT d_sync FROM sync');
       $stmt->execute();
       $rows = $stmt->fetch(PDO::FETCH_ASSOC);
     }
-    if ($_POST['id']==1){
+    if ($id==1){
       $stmt = $db->prepare('SELECT loc FROM perso WHERE equipe=(SELECT equipe FROM perso WHERE id=:id)');
-      $stmt->bindParam(':id', $_POST['id']);
+      $stmt->bindParam(':id', $id);
       $stmt->execute();
       $stmt->setFetchMode(PDO::FETCH_ASSOC);
       $rows = $stmt->fetchAll();
@@ -27,18 +26,17 @@ try {
         $xg=$xg+substr($row['loc'],1,strpos($row['loc'],",")-1);
         $yg=$yg+substr($row['loc'],strpos($row['loc'],",")+1,strpos($row['loc'],")")-1);
       }
-      $stmt = $db->prepare('INSERT INTO qg (capa,equipe,qg_bat,loc,partie) VALUES (1000,(SELECT equipe FROM perso WHERE id=:id),TRUE,POINT(:x,:y),:partie)');
-      $stmt->bindParam(':id', $_POST['id']);
+      $stmt = $db->prepare('INSERT INTO qg (capa,equipe,qg_bat,loc) VALUES (1000,(SELECT equipe FROM perso WHERE id=:id),TRUE,POINT(:x,:y))');
+      $stmt->bindParam(':id', $id);
       $xg = (float)$xg/3;
       $yg = (float)$yg/3;
       $stmt->bindParam(':x', $xg);
       $stmt->bindParam(':y', $yg);
-      $stmt->bindParam(':partie', $_POST['partie']);
       $stmt->execute();
     }
-    elseif ($_POST['id']==2){
+    elseif ($id==2){
       $stmt = $db->prepare('SELECT loc FROM perso WHERE equipe=(SELECT equipe FROM perso WHERE id=:id)');
-      $stmt->bindParam(':id', $_POST['id']);
+      $stmt->bindParam(':id', $id);
       $stmt->execute();
       $stmt->setFetchMode(PDO::FETCH_ASSOC);
       $rows = $stmt->fetchAll();
@@ -46,26 +44,26 @@ try {
         $xg=$xg+substr($row['loc'],1,strpos($row['loc'],",")+1);
         $yg=$yg+substr($row['loc'],strpos($row['loc'],",")+1,strpos($row['loc'],")")+1);
       }
-      $stmt = $db->prepare('INSERT INTO qg (capa,equipe,qg_bat,loc,partie) VALUES (1000,(SELECT equipe FROM perso WHERE id=:id),TRUE,POINT(:x,:y),:partie)');
-      $stmt->bindParam(':id', $_POST['id']);
+      $stmt = $db->prepare('INSERT INTO qg (capa,equipe,qg_bat,loc) VALUES (1000,(SELECT equipe FROM perso WHERE id=:id),TRUE,POINT(:x,:y))');
+      $stmt->bindParam(':id', $id);
       $xg = (float)$xg/3;
       $yg = (float)$yg/3;
       $stmt->bindParam(':x', $xg);
       $stmt->bindParam(':y', $yg);
-      $stmt->bindParam(':partie', $_POST['partie']);
       $stmt->execute();
     }
     else{
       sleep(2);
     }
     $stmt = $db->prepare('SELECT loc FROM qg WHERE equipe=(SELECT equipe FROM perso WHERE id=:id)');
-    $stmt->bindParam(':id', $_POST['id']);
+    $stmt->bindParam(':id', $id);
     $stmt->execute();
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
     $locqg = $stmt->fetchAll();
+	$envoi=Array($locqg,$id);
     $stmt = $db->prepare('UPDATE sync SET d_sync=0');
     $stmt->execute();
-    echo json_encode($locqg);
+    echo json_encode($envoi);
   } catch (Exception $e) {
       echo "<h1 align='center'>Error about the start!</h1>";
       echo $e;
